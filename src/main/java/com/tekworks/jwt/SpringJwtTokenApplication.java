e0d038cf1fb04b9e9c7593b2961ecc577d1e6c0a
Yash-123patel <patelyash250702@gmail.com>
2025-01-22 17:20:54 +0530

    Initial commit

---
+++ src/main/java/com/tekworks/jwt/SpringJwtTokenApplication.java
@@ -0,0 +1,13 @@
+package com.tekworks.jwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJwtTokenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJwtTokenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tekworks/jwt/SpringJwtTokenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tekworks.jwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJwtTokenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
